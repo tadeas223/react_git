ba83854253259929f1543bad1d1039c3688386df
Tadeas Maca <maca.tadeas@gmail.com>
2025-02-15 21:17:17 +0100

    AgeCalculator :)

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AgeCalculator from './AgeCalculator'
 
 function App() {
   const [count, setCount] = useState(0)
- 
+
   return (
     <>
       <div>
@@ -21,6 +22,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <AgeCalculator />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
